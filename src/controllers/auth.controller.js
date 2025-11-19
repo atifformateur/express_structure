a067e091d39ca4543b3766b8dd8a32ddfa92dae3
atifformateur <atif.zourgani@laplateforme.io>
2025-11-19 11:16:16 +0100

    controller register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,41 @@
 const authService = require('../services/auth.service');
 const { validationResult } = require('express-validator');
 
-exports.register = (req, res) => {
-    console.log('test register')
+exports.register = async (req, res) => {
+    //recupere les erreurs generer par registerRules
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        //stop si jamais payload invalide
+        return res.status(400).json({
+            success:false,
+            errors: errors.array()
+        })
+    }
+    try {
+        // get infos et les enregistrer en bdd
+        const user = await authService.register(req.body);
+        return res.status(201).json({
+            success:true,
+            message: 'user crée avec succes',
+            data: {
+                id: user.id,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        if(error.name === 'SequelizeUniqueConstraintError'){
+            return res.status(409).json({
+                success:false,
+                message:'clé unique mail deja utilisé',
+                data:null
+            })
+        }
+        return res.status(500).json({
+            success: false,
+            message:'erreur interne',
+            data:null
+        })
+    }
 }
 
 exports.login = (req, res) => {
